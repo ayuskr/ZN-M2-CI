f01b1c21d3ec80bd57184ab0ba430dc69d323b80
ayuskr <101887670+ayuskr@users.noreply.github.com>
2026-04-09 21:09:59 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -1,1 +1,25 @@
 
+#!/bin/bash
+
+UPDATE_PACKAGE() {
+    NAME=$1
+    REPO=$2
+    BRANCH=$3
+
+    rm -rf package/$NAME
+    git clone --depth=1 -b $BRANCH https://github.com/$REPO.git package/$NAME
+}
+
+cd wrt
+
+echo "==== Custom Packages ===="
+
+# Passwall全套（必须）
+UPDATE_PACKAGE passwall-packages xiaorouji/openwrt-passwall-packages main
+UPDATE_PACKAGE passwall xiaorouji/openwrt-passwall main
+
+# MosDNS（稳定版）
+UPDATE_PACKAGE mosdns sbwml/luci-app-mosdns v5
+
+# Lucky
+UPDATE_PACKAGE lucky gdy666/luci-app-lucky main
