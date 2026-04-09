140ac5e67f8fa01770ed064ad4e89ca8cc3e3609
ayuskr <101887670+ayuskr@users.noreply.github.com>
2026-04-09 21:10:17 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -1,1 +1,20 @@
 
+#!/bin/bash
+
+cd wrt
+
+echo "==== Remove WiFi ===="
+
+# 干掉WiFi（彻底）
+sed -i '/ath11k/d' .config
+sed -i '/wpad/d' .config
+sed -i '/hostapd/d' .config
+
+echo "==== System Optimize ===="
+
+# 精简
+sed -i '/ppp/d' .config
+sed -i '/ipv6/d' .config
+
+# 防止143（减少编译压力）
+echo "CONFIG_DEVEL=y" >> .config
